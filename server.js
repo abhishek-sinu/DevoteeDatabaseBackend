98d912662b88d7edd1108c51e5755483b26e4bb3
abhishek-sinu <57484378+abhishek-sinu@users.noreply.github.com>
2025-09-23 20:43:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,13 @@
 
   dotenv.config();
   const app = express();
+  const cors = require('cors');
 
-  app.use(cors());
+  app.use(cors({
+    origin: 'https://devotee-database-frontend.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
   app.use(express.json());
   app.use("/api", authRoutes);
   app.use("/uploads", express.static("uploads"));
