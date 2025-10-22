05a6a75845e9896a82f2d40a0803454f145ea8b4
Abhishek Das <abhi.sinu.1@gmail.com>
2025-10-22 22:01:23 +0530

    Update publicDevotee.js

--- routes/publicDevotee.js
+++ routes/publicDevotee.js
@@ -4,7 +4,6 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import ExcelJS from 'exceljs';
-import path from 'path';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
